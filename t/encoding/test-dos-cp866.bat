34b5ed647d85adfec7cd5a6b58a4d0ff7b891582
ildar-shaimordanov <ildar.shaimordanov@gmail.com>
2021-12-15 11:45:12 +0300

    reorder tests

---
+++ t/encoding/test-dos-cp866.bat
@@ -0,0 +1,6 @@
+@echo off
+
+:: this file MUST BE in cp866
+:: encoding for title MUST BE cp1251
+
+perl "../../git-md-toc" --title-transcode=cp1251 -ut "[Сказки Пушкина]" -Tutf8 < "utf8-cyrillic.md"
